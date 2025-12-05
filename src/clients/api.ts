1358a5a16944e094421557fd3fff8ac5b17690bc
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-05 13:25:55 -0800

    feat: update env variables

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
     Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFmZjdmOTg1NWIzY2Q0M2U4ZWM0MCIsInVzZXJuYW1lIjoidXNlcjEyMyIsImVtYWlsIjoidXNlcjEyM0BnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjQ5NTYyODUsImV4cCI6MTc2NDk2MzQ4NX0.TQRiMqqDKqMCrBLGr11xIP191UZv9HJAqXv8JiDZDqo`,
   },
