64d5024b7d89baf545cf4dcb3d05c35d1fcc49e7
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-08 11:20:14 -0800

    fix: update token

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: {
-    Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFmZjdmOTg1NWIzY2Q0M2U4ZWM0MCIsInVzZXJuYW1lIjoidXNlcjEyMyIsImVtYWlsIjoidXNlcjEyM0BnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjQ5NzQ0MjEsImV4cCI6MTc2NTA2MDgyMX0.Q1IA_yt45dKrJDDCeSsIPNO1y6iB__L6gJUlm3z5INE`,
+    Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFmZjdmOTg1NWIzY2Q0M2U4ZWM0MCIsInVzZXJuYW1lIjoidXNlcjEyMyIsImVtYWlsIjoidXNlcjEyM0BnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUyMTkwOTIsImV4cCI6MTc2NTMwNTQ5Mn0.4f3oYHoJnRaagSa6m-ccrHLwbLwgLV_6cAMXdvlWFO4`,
   },
 });
