0b903232eeb969a46b59c481f8eb05dbc4486023
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-11 02:01:48 -0800

    refactor: update 404 page with new styles

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,9 +6,15 @@ export const NotFoundPage = () => {
       <h1 className='text-4xl font-bold text-gray-800 mb-6'>
         404 - Page Not Found
       </h1>
-      <p className='text-lg text-gray-600 mb-8 max-w-md'>
+
+      <img
+        src='https://www.boredpanda.com/blog/wp-content/uploads/2024/10/bugs-bunny-no-a-classic-cartoon-meme-explained-cover_675.jpg'
+        alt='404'
+        className='w-[600px] rounded-lg shadow-lg mb-8'
+      />
+      {/* <p className='text-lg text-gray-600 mb-8 max-w-md'>
         Sorry, the page you are looking for does not exist or has been moved.
-      </p>
+      </p> */}
       <Link
         to='/'
         className='inline-block bg-blue-600 hover:bg-blue-700 text-white font-medium py-3 px-6 rounded-lg transition-colors duration-200 shadow-md'
